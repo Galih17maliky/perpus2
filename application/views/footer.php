66578fe15831642bbaa0eac0e38d0ff343b4d9a8
galih17maliky <galih02maliky@gmail.com>
2020-12-14 00:03:25 +0700

    Initial commit

---
+++ application/views/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="copyright">
+            Copyright &copy;2020, Di Buat Dengan <i class="fa fa-heart heart text-danger"></i> Oleh <a href="#">Galih Ricksa Maliky</a>
+        </div>              
+    </div>
+</footer>
